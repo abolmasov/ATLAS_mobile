51fb683f7dfb31ebf08c274a420d38e9e85fb229
Roman <sillencium@gmail.com>
2017-11-17 12:47:49 +0100

    Updae send request

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -15,8 +15,8 @@ export class LoginPage {
   // If you're using the username field with or without email, make
   // sure to add it to the type
   account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
+    email: 'mbrodetskaya@archer-soft.com',
+    password: 'kop4R4fd1UTtI'
   };
 
   // Our translated text strings
@@ -37,7 +37,7 @@ export class LoginPage {
     this.user.login(this.account).subscribe((resp) => {
       this.navCtrl.push(MainPage);
     }, (err) => {
-      this.navCtrl.push(MainPage);
+      //this.navCtrl.push(MainPage);
       // Unable to log in
       let toast = this.toastCtrl.create({
         message: this.loginErrorString,
